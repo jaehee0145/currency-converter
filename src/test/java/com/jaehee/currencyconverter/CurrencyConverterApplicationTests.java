3828173805075631fe7eb11f29d64ebc1f77c9f5
jaehee0145 <jaeheepark0145@gmail.com>
2019-06-03 23:16:53 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/jaehee/currencyconverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jaehee.currencyconverter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyConverterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
