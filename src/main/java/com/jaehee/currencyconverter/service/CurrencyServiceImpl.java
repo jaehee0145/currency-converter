985452ebee2fc9601519b19b46c01f0d4537799a
jaehee0145 <jaeheepark0145@gmail.com>
2019-06-04 10:51:40 +0900

    feat: 외부 API 호출해서 DTO로 받기

--- src/main/java/com/jaehee/currencyconverter/service/CurrencyServiceImpl.java
+++ src/main/java/com/jaehee/currencyconverter/service/CurrencyServiceImpl.java
@@ -1,10 +1,19 @@
 package com.jaehee.currencyconverter.service;
 
 import com.jaehee.currencyconverter.dto.CurrencyDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+
+@RequiredArgsConstructor
 public class CurrencyServiceImpl implements CurrencyService{
+    private final RestTemplate restTemplate;
+
     @Override
     public CurrencyDto getCurrencyDto() {
-        return null;
+        String url = "http://apilayer.net/api/live?access_key=2ce8264e0ef3cfe017c7f254d68b65ad";
+        CurrencyDto currencyDto = restTemplate.getForObject(url, CurrencyDto.class);
+        return currencyDto;
     }
 }
